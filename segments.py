01bdccfe342975967a764adec2098e187e2b942f
Derek Stratton <derekdstratton@gmail.com>
2021-01-04 14:08:59 -0800

    Tidying up the code

--- segments.py
+++ segments.py
@@ -1,14 +1,9 @@
-from pytube import YouTube
-import cv2
-import numpy
-
-# getting ffmpeg to work on wsl, building ope
-# ncv for openface: https://github.com/justadudewhohacks/opencv4nodejs/issues/274
 import os
 import pandas as pd
 import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 def extract_features(video_path):
     base_path = os.path.basename(video_path)
@@ -23,47 +18,14 @@ def extract_features(video_path):
         subprocess.run([openface_build_path + "/FeatureExtraction", "-f", video_path])
     else:
         print("csv file already exists, loading into df")
-    df = pd.read_csv(csv_from_video_path)
-    return df
+    return pd.read_csv(csv_from_video_path)
 
-# one idea is to take a big rolling average (N=len(x)) as the baseline
-# then compute the standard deviation. this makes assumptions that there's
-# a distribution of the data that's centered around the mean, which may
-# not be accurate... my goal is to detect anomalous signals
+
 def plot_au_over_time(df, au_num, rolling_mean_window=20):
     plt.plot(df['frame'], df['AU' + f'{au_num:02}' + '_r'])
     x = df['AU' + f'{au_num:02}' + '_r']
     df['interesting_segments' + 'AU' + f'{au_num:02}'] = plot_with_rolling(df['frame'], x, rolling_mean_window)
 
-
-    # # smooths it out a little bit.
-    # rolling_mean = np.convolve(x, np.ones(rolling_mean_window) / rolling_mean_window, mode='same')
-    #
-    # # the full window gives something a bit clearer.
-    # rolling_mean_with_full_window = np.convolve(x, np.ones(len(x)) / len(x), mode='same')
-    #
-    # # plot the moving average
-    # plt.plot(df['frame'], rolling_mean)
-    # plt.plot(df['frame'], rolling_mean_with_full_window)
-    # plt.plot(df['frame'], rolling_mean_with_full_window + deviation)
-    # plt.show()
-    # # AU 6 deviates from the mean really nicely except for surprise
-    # # we can maybe generalize it to go thru all the AUs, and find
-    # # all segments that deviate from each's distribution of "normal"
-    # # to find the emotions. 25 captures surpise well.
-    # df['interesting_segments' + 'AU' + f'{au_num:02}'] = rolling_mean > rolling_mean_with_full_window + deviation
-    # https://dsp.stackexchange.com/questions/48508/how-detect-signal-from-noise
-    # https://en.wikipedia.org/wiki/Forecasting
-    # https://en.wikipedia.org/wiki/Time_series
-    # this can dive in deep
-    # https://stackoverflow.com/questions/13728392/moving-average-or-running-mean
-    # try plotting the overall mean and sd's, rolling mean
-    # you could compare the actual values or rolling vals to determine sd
-    # todo: try pca to get au combinations, rather than saying "for all AUs find the intersection of useful segments. which might be good idk"
-    # it might be useful to characterize segments by the number of AU matches, and
-    # the length of the segments (1 match may not be useful) (small segments may
-    # or may not be useful). You may want to just manually delete segments that
-    # show up though)
 
 def plot_with_rolling(frame, au_over_time, rolling_mean_window):
     deviation = np.std(au_over_time)
@@ -71,42 +33,40 @@ def plot_with_rolling(frame, au_over_time, rolling_mean_window):
     rolling_mean = np.convolve(au_over_time, np.ones(rolling_mean_window) / rolling_mean_window, mode='same')
 
     # the full window gives something a bit clearer.
-    # rolling_mean_with_full_window = np.convolve(au_over_time, np.ones(len(au_over_time)) / (len(au_over_time)), mode='same')
-    rolling_mean_with_full_window = np.convolve(au_over_time, np.ones(len(au_over_time)//2) / (len(au_over_time)//2), mode='same')
+    # do I want to divide by 2?
+    rolling_mean_with_full_window = np.convolve(au_over_time,
+                                                np.ones(len(au_over_time) // 2) / (len(au_over_time) // 2), mode='same')
 
     # https://stackoverflow.com/questions/46964363/filtering-out-outliers-in-pandas-dataframe-with-rolling-median
-
 
     # plot the moving average
     plt.plot(frame, rolling_mean)
     plt.plot(frame, rolling_mean_with_full_window)
     plt.plot(frame, rolling_mean_with_full_window + deviation)
-    # plt.plot(df['frame'], rolling_mean)
-    # plt.plot(df['frame'], rolling_mean_with_full_window)
-    # plt.plot(df['frame'], rolling_mean_with_full_window + deviation)
     plt.show()
     deviation = np.std(au_over_time)
-    return rolling_mean > rolling_mean_with_full_window + 2* deviation
+    return rolling_mean > rolling_mean_with_full_window + 2 * deviation
 
 
 def logic_on_au_features(df):
     # https://stackoverflow.com/questions/11285613/selecting-multiple-columns-in-a-pandas-dataframe
     # NOTE: i'm deciding to remove au45 since it's acting weird
     # (guess since i'm slicing this may also omit 26?)
-    b=df.loc[:, 'interesting_segmentsAU01':'interesting_segmentsAU26']
-    c=b.any(axis=1)
+    b = df.loc[:, 'interesting_segmentsAU01':'interesting_segmentsAU26']
+    c = b.any(axis=1)
     df['interesting_segments'] = c
     plt.plot(df['frame'], c)
     plt.show()
 
+
 # https://medium.com/@ansjin/dimensionality-reduction-using-pca-on-multivariate-timeseries-data-b5cc07238dc4
 def pca_on_au_features(df):
-    b=df.loc[:, 'AU01_r':'AU26_r']
+    b = df.loc[:, 'AU01_r':'AU26_r']
     from sklearn.decomposition import PCA
     pca = PCA(n_components=4)
     return pca.fit_transform(np.array(b))
 
-from videos import *
+
 if __name__ == "__main__":
     video_basename = "simple_test.mp4"
     video_name = "videos/" + video_basename
@@ -118,20 +78,22 @@ if __name__ == "__main__":
     # plot_au_over_time(df, 25, 1041)
 
     # df2 = df.iloc[4000:6000,]
+    au_list = [1, 2, 4, 5, 6, 7, 9, 10, 12, 14, 15, 17, 20, 23, 25, 26, 45]
     df2 = df
     for au in au_list:
         # plot_au_over_time(df, au, 20)
         plot_au_over_time(df2, au, rolling_mean_window=50)
         # kinda like the bigger windows for not catching spikes
     logic_on_au_features(df2)
-    interestings = df2['interesting_segments'][df2['interesting_segments']==True].index
+    interestings = df2['interesting_segments'][df2['interesting_segments'] == True].index
     # https://stackoverflow.com/questions/2361945/detecting-consecutive-integers-in-a-list
 
     from itertools import groupby
     from operator import itemgetter
+
     segments = []
     # look for consecutive chains of true
-    for k, g in groupby(enumerate(interestings), lambda ix : ix[0] - ix[1]):
+    for k, g in groupby(enumerate(interestings), lambda ix: ix[0] - ix[1]):
         segments.append(list(map(itemgetter(1), g)))
 
     seglens = [len(x) for x in segments]
@@ -145,4 +107,3 @@ if __name__ == "__main__":
 
     seg_df = pd.DataFrame(data, columns=['start_frame', 'end_frame', 'label'])
     seg_df.to_csv("segment_labels/" + video_basename.split('.')[0] + ".csv")
-    # replacing larger signal values to get a better idea of the main curve
--- videos.py
+++ videos.py
@@ -1,22 +1,12 @@
+import os
 from pytube import YouTube
 import cv2
-import numpy
 
-# getting ffmpeg to work on wsl, building ope
-# ncv for openface: https://github.com/justadudewhohacks/opencv4nodejs/issues/274
-import os
-import pandas as pd
-import subprocess
-import matplotlib.pyplot as plt
-import numpy as np
-
-# the interest-times are VERY approximate eyeballing
 video_path = "videos/simple_test.mp4"
-au_list = [1,2,4,5,6,7,9,10,12,14,15,17,20,23,25,26,45]
+
+
 # os.environ['DISPLAY'] = "localhost:0.0"
 # print(os.environ['DISPLAY'])
-
-# todo: make an if to check if it's already processed, with a separate function
 
 # ask for the link from user, and download it
 # returns mp4 filename
@@ -45,11 +35,11 @@ def try_download(link):
         print("File already exists")
     return ys.default_filename.replace(" ", "_")
 
+
 # https://www.geeksforgeeks.org/python-play-a-video-using-opencv/
 def play(video_path, start_frame=0, end_frame=None):
     cap = cv2.VideoCapture(video_path)
     # Create a VideoCapture object and read from input file
-    #
     # Check if camera opened successfully
     # https://subscription.packtpub.com/book/application_development/9781788474443/1/ch01lvl1sec24/jumping-between-frames-in-video-files
     cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
@@ -85,57 +75,3 @@ def play(video_path, start_frame=0, end_frame=None):
     # Closes all the frames
     cv2.destroyAllWindows()
     return cap
-
-# maybe we need some sort of way to rank the AUs.
-# i should probably go thru a messy youtube video (with talking) and find some parts to extract...
-# and then see if i can get it to find them.
-# (talking is the most awkward part with AU detection)
-# maybe make AU thresholds different for those parts
-
-# note, not all vids have caption data
-# 1 simple way to handle it is just to not consider voice parts
-# this gets tricky with multiple people
-# caption = yt.captions.get_by_language_code('en')
-# temp = caption.generate_srt_captions()
-# you can get the frames by multiplying ys.fps by the time in seconds from srt
-# segment lengths are also really varied. they can be way too
-# long or way too short
-
-
-from segments import *
-
-if __name__ == "__main__":
-    video_path2 = "https://www.youtube.com/watch?v=b0GxmVCd-FE"
-    # video name needs renamed something that doesn't have spaces
-    video_name = try_download(video_path2)
-    df = extract_features(video_name)
-    # z = play(video_path)
-
-    # c = pca_on_au_features(df)
-
-    # plot_au_over_time(df, 25, 1041)
-
-    # todo: make a for loop to graph all the au's
-    df2 = df.iloc[4000:6000,]
-    for au in au_list:
-        # plot_au_over_time(df, au, 20)
-        plot_au_over_time(df2, au, rolling_mean_window=50)
-        # kinda like the bigger windows for not catching spikes
-    logic_on_au_features(df2)
-    interestings = df2['interesting_segments'][df2['interesting_segments']==True].index
-    # https://stackoverflow.com/questions/2361945/detecting-consecutive-integers-in-a-list
-
-    from itertools import groupby
-    from operator import itemgetter
-    segments = []
-    for k, g in groupby(enumerate(interestings), lambda ix : ix[0] - ix[1]):
-        segments.append(list(map(itemgetter(1), g)))
-
-    seglens = [len(x) for x in segments]
-    # maybe if len less than 50, it's just noise
-    for seg in segments:
-        print(seg[0], seg[-1])
-        play("cringe.mp4", seg[0], seg[-1])
-
-
-    # replacing larger signal values to get a better idea of the main curve
