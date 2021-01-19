d3dbc982ee6dfb3b120a73b71b6188619fb40e76
Derek Stratton <derekdstratton@gmail.com>
2021-01-18 16:23:52 -0800

    colored labels

--- labeling.py
+++ labeling.py
@@ -2,10 +2,6 @@ import os
 
 import pandas as pd
 
-video_path = "videos/simple_test.mp4"
-segments_path = "segment_labels/simple_test.csv"
-segments = pd.read_csv(segments_path)
-
 #https://pythonprogramminglanguage.com/pyqt5-video-widget/
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
@@ -15,6 +11,13 @@ from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
 from PyQt5.QtWidgets import QMainWindow,QWidget, QPushButton, QAction
 from PyQt5.QtGui import QIcon
 import sys
+
+class EmotionButton(QPushButton):
+    def __init__(self, name, styleSheet=None):
+        super().__init__(name)
+        if styleSheet is not None:
+            self.setStyleSheet(styleSheet)
+
 
 class VideoWindow(QMainWindow):
 
@@ -77,6 +80,55 @@ class VideoWindow(QMainWindow):
         layout.addLayout(controlLayout)
         layout.addWidget(self.errorLabel)
 
+        hbox = QHBoxLayout()
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(EmotionButton("Amusement", styleSheet="background-color: yellow"))
+        vbox1.addWidget(EmotionButton("Excitement", styleSheet="background-color: yellow"))
+        vbox1.addWidget(EmotionButton("Surprise", styleSheet="background-color: yellow"))
+        vbox1.addWidget(EmotionButton("Pride", styleSheet="background-color: cyan"))
+        vbox1.addWidget(EmotionButton("Love", styleSheet="background-color: hotpink"))
+        vbox1.addWidget(EmotionButton("Sexual Desire", styleSheet="background-color: hotpink"))
+
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(EmotionButton("Contentment", styleSheet="background-color: green"))
+        vbox2.addWidget(EmotionButton("Relief", styleSheet="background-color: green"))
+        vbox2.addWidget(EmotionButton("Happiness", styleSheet="background-color: green"))
+        vbox2.addWidget(EmotionButton("Sadness", styleSheet="background-color: blue"))
+        vbox2.addWidget(EmotionButton("Disappointment", styleSheet="background-color: blue"))
+        vbox2.addWidget(EmotionButton("Guilt", styleSheet="background-color: blue"))
+
+        vbox3 = QVBoxLayout()
+        vbox3.addWidget(EmotionButton("Anger", styleSheet="background-color: red"))
+        vbox3.addWidget(EmotionButton("Contempt", styleSheet="background-color: red"))
+        vbox3.addWidget(EmotionButton("Pain", styleSheet="background-color: red"))
+        vbox3.addWidget(EmotionButton("Jealousy", styleSheet="background-color: purple"))
+        vbox3.addWidget(EmotionButton("Disgust", styleSheet="background-color: purple"))
+
+        vbox4 = QVBoxLayout()
+        vbox4.addWidget(EmotionButton("Awkwardness", styleSheet="background-color: orange"))
+        vbox4.addWidget(EmotionButton("Confusion", styleSheet="background-color: orange"))
+        vbox4.addWidget(EmotionButton("Embarassment", styleSheet="background-color: orange"))
+        vbox4.addWidget(EmotionButton("Fear", styleSheet="background-color: orange"))
+        vbox4.addWidget(EmotionButton("Nervousness", styleSheet="background-color: orange"))
+
+
+        vbox5 = QVBoxLayout()
+        vbox5.addWidget(EmotionButton("Neutral", styleSheet="background-color: gray"))
+        vbox5.addWidget(EmotionButton("Boredom", styleSheet="background-color: gray"))
+        vbox5.addWidget(EmotionButton("Tired", styleSheet="background-color: gray"))
+        vbox5.addSpacing(20)
+        vbox5.addWidget(EmotionButton("BAD CLIP"))
+
+        hbox.addLayout(vbox1)
+        hbox.addLayout(vbox2)
+        hbox.addLayout(vbox3)
+        hbox.addLayout(vbox4)
+        hbox.addLayout(vbox5)
+
+        layout.addLayout(hbox)
+
+        # layout.addWidget(EmotionButton("LOL"))
+
         # Set widget to contain window contents
         wid.setLayout(layout)
 
@@ -85,6 +137,10 @@ class VideoWindow(QMainWindow):
         self.mediaPlayer.positionChanged.connect(self.positionChanged)
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
         self.mediaPlayer.error.connect(self.handleError)
+
+        video_path = "videos/simple_test.mp4"
+        segments_path = "segment_labels/simple_test.csv"
+        self.segments = pd.read_csv(segments_path)
 
         # open the media and segments
         # needs an absolute path
@@ -103,6 +159,7 @@ class VideoWindow(QMainWindow):
             self.mediaPlayer.setMedia(
                     QMediaContent(QUrl.fromLocalFile(fileName)))
             self.playButton.setEnabled(True)
+        self.segments = pd.read_csv("segment_labels/" + os.path.basename(fileName).split('.')[0] + ".csv")
 
     def exitCall(self):
         sys.exit(app.exec_())
@@ -136,8 +193,8 @@ class VideoWindow(QMainWindow):
 
     def on_click(self):
         print('button hit')
-        startframe = segments['start_frame'][self.segment_counter]
-        endframe = segments['end_frame'][self.segment_counter]
+        startframe = self.segments['start_frame'][self.segment_counter]
+        endframe = self.segments['end_frame'][self.segment_counter]
         # arg is milliseconds (assuming video is run at 30 fps)
         frames_to_millisecs = startframe // 30 * 1000
         self.mediaPlayer.setPosition(frames_to_millisecs)
@@ -147,7 +204,7 @@ class VideoWindow(QMainWindow):
         self.mediaPlayer.pause()
 
         self.segment_counter = self.segment_counter + 1
-        if self.segment_counter >= len(segments):
+        if self.segment_counter >= len(self.segments):
             self.segment_counter = 0
 
 if __name__ == '__main__':
