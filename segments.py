eadcec3e7bfc8b4b65dd087358df069d2bd18e56
Derek Stratton <derekdstratton@gmail.com>
2021-01-26 13:33:00 -0800

    just found out frame != index in the dataframe, tryin to fix that

--- segments.py
+++ segments.py
@@ -19,17 +19,24 @@ def extract_features(video_path):
         openface_build_path = os.environ['OPENFACE_PATH']
 
         # for a single person
-        os.system(openface_build_path + "/FeatureExtraction")
+        # os.system(openface_build_path + "/FeatureExtraction")
+        # os.system(openface_build_path + "/FeatureExtraction")
 
         # https://github.com/TadasBaltrusaitis/OpenFace/wiki/Command-line-arguments
-        subprocess.run([openface_build_path + "/FeatureExtraction", "-f", video_path])  # , "-aus"])
+        # subprocess.run([openface_build_path + "/FeatureExtraction", "-f", video_path])  # , "-aus"])
+        subprocess.run([openface_build_path + "/FaceLandmarkVidMulti", "-f", video_path])  # , "-aus"])
     else:
         print("csv file already exists, loading into df")
     full_df = pd.read_csv(csv_from_video_path)
     # num_frames x num_AUs dataframe containing the AU intensity for each video frame
     # note: you can change the first slice here to look at a subset of video frames
     # note: i'm currently removing au 45 (blinking), it gave me worse results.
-    return full_df.loc[:, 'AU01_r':'AU26_r']
+    col_indices = list(range(list(full_df.columns).index('AU01_r'), list(full_df.columns).index('AU26_r')+1))
+    col_indices.append(0)
+    col_indices.append(1)
+    id0 = full_df.loc[full_df['face_id'] == 0]
+    return id0.iloc[:, col_indices]
+    # return full_df.loc[:, 'AU01_r':'AU26_r']
 
 
 # segment finding method 1
@@ -44,7 +51,7 @@ def find_where_rolling_mean_deviates_from_threshold(au_df):
 
     # plots of the AUs are useful
     for col in smoothed_au_df.columns:
-        plot_au(smoothed_au_df[col], col, "Smoothed AU Plot: " + col)
+        plot_au(smoothed_au_df['frame'], smoothed_au_df[col], col, "Smoothed AU Plot: " + col)
 
     # num_frames x num_AUs boolean dataframe, where it's true if the smoothed value is > 1 std away from median
     au_deviants_df = pd.DataFrame()
@@ -72,23 +79,23 @@ def find_segments_from_clusters(au_df):
     from sklearn.decomposition import PCA
     pca = PCA(n_components=8)
     # num_frames x num_components array
-    aus_transformed = pca.fit_transform(au_df)
+    aus_transformed = pca.fit_transform(au_df.loc[:, 'AU01_r':'AU26_r'])
     # how many dimensions (components) is reasonable to cluster?
 
     from sklearn import cluster
     # k means: i think this is just worse than agglomerative
-    # kmeans = cluster.KMeans()  # default is 8 clusters
+    # kmeans = cluster.KMeans(2)  # default is 8 clusters
     # kmeans.fit(aus_transformed)
     # # num_frames length array with each value being from 0 to num_groups - 1
     # group_for_each_frame = kmeans.predict(aus_transformed)
-    # plot_groups(group_for_each_frame)
 
     # agglomerative: the distance_threshold seems very sensitive
     agg = cluster.AgglomerativeClustering(n_clusters=None, distance_threshold=20)
     agg.fit(aus_transformed)
     # num_frames length array with each value being from 0 to num_groups - 1
     group_for_each_frame = agg.labels_
-    plot_groups(group_for_each_frame)
+
+    plot_groups(au_df['frame'], group_for_each_frame)
 
     # one problem with clusters is knowing which cluster(s) are interesting or not
     # i'm just saying if it's not in the most common group, it's interesting.
@@ -96,14 +103,14 @@ def find_segments_from_clusters(au_df):
     not_in_most_common_group = group_for_each_frame != most_common_group
 
     # plots where segments are based on if any au deviates from median
-    plot_segment_or_not(not_in_most_common_group)
+    plot_segment_or_not(au_df['frame'], not_in_most_common_group)
 
-    return segment_or_not_to_dataframe(not_in_most_common_group, play_segs=False)
+    return segment_or_not_to_dataframe(au_df['frame'],not_in_most_common_group, play_segs=True)
 
 
 # transforms a num_frames length bool series to a num_segments x 3 segments dataframe
 # play_segs will play the segments it finds if true
-def segment_or_not_to_dataframe(seg_or_not_series, play_segs=False):
+def segment_or_not_to_dataframe(frame, seg_or_not_series, play_segs=False):
     # https://stackoverflow.com/questions/7352684/how-to-find-the-groups-of-consecutive-elements-in-a-numpy-array
     def consecutive(arr, stepsize=1):
         return np.split(arr, np.where(np.diff(arr) != stepsize)[0] + 1)
@@ -115,9 +122,11 @@ def segment_or_not_to_dataframe(seg_or_not_series, play_segs=False):
         import videos
 
     for seg in list_of_segment_frames:
-        data.append((seg[0], seg[-1], 0))
+        startF = frame.iloc[seg[0]]
+        endF = frame.iloc[seg[-1]]
+        data.append((startF, endF, 0))
         if play_segs:
-            videos.play(video_name, seg[0], seg[-1])
+            videos.play(video_name, startF, endF)
 
     return pd.DataFrame(data, columns=['start_frame', 'end_frame', 'label'])
 
@@ -139,18 +148,18 @@ def delete_segments_by_length(seg_df, smallest_len=20, largest_len=200):
     return seg_df[(seg_lens >= smallest_len) & (seg_lens <= largest_len)]
 
 
-def plot_segment_or_not(seg_or_not_series):
-    plt.plot(seg_or_not_series)
+def plot_segment_or_not(frame, seg_or_not_series):
+    plt.plot(frame, seg_or_not_series)
     plt.xlabel("frame")
     plt.ylabel("segment or not")
     plt.title("Detected Segments for each Frame")
     plt.show()
 
 
-def plot_au(au_series, au_name, title):
-    plt.plot(au_series, label=au_name)
-    plt.plot(np.full(len(au_series), au_series.median()), label="median")
-    plt.plot(np.full(len(au_series), au_series.median() + au_series.std()), label="median plus 1 std")
+def plot_au(frame, au_series, au_name, title):
+    plt.plot(frame, au_series, label=au_name)
+    plt.plot(np.full(len(frame), au_series.median()), label="median")
+    plt.plot(np.full(len(frame), au_series.median() + au_series.std()), label="median plus 1 std")
     plt.xlabel("frame")
     plt.ylabel(au_name)
     plt.title(title)
@@ -158,13 +167,18 @@ def plot_au(au_series, au_name, title):
     plt.show()
 
 
-def plot_groups(groups_series):
-    plt.plot(groups_series)
+def plot_groups(frame, groups_series):
+    plt.plot(frame, groups_series)
     plt.xlabel("frame")
     plt.ylabel("group")
     plt.title("Detected Groups for each Frame")
     plt.show()
 
+def wildwest():
+    # does frame always equal index number? it might not with mia values...
+    # it doesn't...
+    # id0 = full_df.loc[full_df['face_id'] == 0]
+    pass
 
 if __name__ == "__main__":
     # first argument should be the basename of the video
@@ -178,8 +192,8 @@ if __name__ == "__main__":
     au_df = extract_features(video_name)
 
     # plotting the au's
-    # for col in au_df.columns:
-    #     plot_au(au_df[col], col, "AU Plot: " + col)
+    for col in au_df.columns:
+        plot_au(au_df['frame'], au_df[col], col, "AU Plot: " + col)
 
     if int(method_to_run) == 1:
         # method 1 for segments
@@ -190,4 +204,4 @@ if __name__ == "__main__":
 
     seg_lens = summarize_segments(seg_df)
     # output the resulting segments to file
-    # seg_df.to_csv("segment_labels/" + video_basename.split('.')[0] + ".csv")
+    # seg_df.to_csv("segment_labels/" + video_basename.split('.')[0] + ".csv", index=False)
