5d055a76e2be51b71ef3a2710e256030cda80ba4
Derek <derekdstratton@gmail.com>
2021-03-13 16:08:33 -0800

    Some (but not enough) needed code cleanup

--- process.py
+++ process.py
@@ -2,30 +2,6 @@ import videos
 import segments
 import os
 
-def create_face_videos(videos_path):
-    basename = os.path.basename(videos_path)
-    # fullpath = os.path.join(videos_path, video)
-    df = videos.assign_face_ids(videos_path)
-
-    if not os.path.exists("cropped_face_videos"):
-        os.mkdir("cropped_face_videos")
-    if not os.path.exists(os.path.join("cropped_face_videos", basename)):
-        os.mkdir(os.path.join("cropped_face_videos", basename))
-
-    # 3. create a video for each aligned face found
-    aligned_videos_paths = []
-    for id in df['id'].unique():
-        # there may be breaks in frame continuity here (it's actually likely)
-        frame_paths = df[df['id'] == id]['path']
-        # the frames from the original video may be useful or necessary
-        # frames = df[df['id'] == id]['frame']
-        # if len(frame_paths) > 100: # todo: this doesnt always apply...
-        if len(frame_paths) > 30:
-            out_path = os.path.join("cropped_face_videos", basename) + "/" + str(int(id)) + ".mp4"
-            # out_path = "videos/" + basename + "_aligned_" + str(int(id)) + ".mp4"
-            videos.make_aligned_video(frame_paths, out_path)
-            aligned_videos_paths.append(out_path)
-
 # 0. downloading videos? change frame rate?
 original_videos_directory = "original_videos"
 face_images_basepath = "cropped_face_detections"
@@ -47,8 +23,7 @@ for video in os.listdir(original_videos_directory):
     # 2. Make a video for each face in the video, and output to a folder of videos
     face_videos_path = os.path.join(face_videos_basepath, videoname_noext)
     if not os.path.exists(face_videos_path):
-        create_face_videos(face_detections_path)
-        # todo: move this function to "videos.py"
+        videos.create_face_videos(face_detections_path)
         assert os.path.exists(face_videos_path)
 
     # 3. Find expression segments for each cropped face video, output to a folder with a video per segment
@@ -65,5 +40,3 @@ for video in os.listdir(original_videos_directory):
         for index, seg in seg_df.iterrows():
             output_video_file = output_path + "/" + str(index) + ".mp4"
             videos.save_sub_video(face_video_file, seg[0], seg[1], output_video_file)
-
-exit(0)
--- videos.py
+++ videos.py
@@ -1,17 +1,10 @@
 import os
-from pytube import YouTube
 import cv2
 
-video_path = "videos/simple_test.mp4"
-
-
-# os.environ['DISPLAY'] = "localhost:0.0"
-# print(os.environ['DISPLAY'])
-
-# ask for the link from user, and download it
-# returns mp4 filename
-# https://towardsdatascience.com/build-a-youtube-downloader-with-python-8ef2e6915d97
+# downloads a YouTube video
 def try_download(link):
+    from pytube import YouTube
+    import demoji
     yt = YouTube(link)
 
     # Showing details
@@ -26,7 +19,6 @@ def try_download(link):
         # Starting download
         print("Downloading...")
         # ys.download()
-        import demoji
         name = ys.title.replace(" ", "_")
         name = demoji.replace(name, "")
         ys.download(output_path="videos", filename=name)
@@ -36,16 +28,17 @@ def try_download(link):
     return ys.default_filename.replace(" ", "_")
 
 
-# video_path: relative path to the video
-def detect_faces(video_path):
+# outputs all images from a video to a file
+# video_path: path to a video file
+# return: path to the image folder of outputs
+def detect_faces(video_path: str) -> str:
+    import face_detection
+
     # pathing
     faces_output_basepath = "cropped_face_detections"
     if not os.path.exists(faces_output_basepath):
         os.mkdir(faces_output_basepath)
     video_basename = os.path.splitext(os.path.basename(video_path))[0]
-    # if not os.path.exists("detections/" + video_basename + "/faces"):
-    #     os.mkdir("detections/" + video_basename)
-    #     os.mkdir("detections/" + video_basename + "/faces")
     faces_video_path = os.path.join(faces_output_basepath, video_basename)
     if not os.path.exists(faces_video_path):
         os.mkdir(faces_video_path)
@@ -54,31 +47,18 @@ def detect_faces(video_path):
         os.mkdir(faces_video_path_inner)
 
     # load face detector and the video
-    import face_detection
-    detector = face_detection.build_detector(
-        "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+    detector = face_detection.build_detector("DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
     cap = cv2.VideoCapture(video_path)
-
-    # Create a VideoCapture object and read from input file
-    # Check if camera opened successfully
-    # https://subscription.packtpub.com/book/application_development/9781788474443/1/ch01lvl1sec24/jumping-between-frames-in-video-files
     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    if (cap.isOpened() == False):
-        print("Error opening video  file")
-
-        # Read until video is completed
-    while (cap.isOpened()):
-
+    if not cap.isOpened():
+        print("Error opening video file")
+    while cap.isOpened():
         # Capture frame-by-frame
         ret, frame = cap.read()
-        # to stop a video, while frame < STOPPING_NUM
-        if ret == True:
-
-            # Display the resulting frame
-            # cv2.imshow('Frame', frame)
-            detections = detector.detect(frame)
+        if ret:
             frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES)
-
+            # the list of detections, equal to amount of faces found in a frame
+            detections = detector.detect(frame)
             for i, face_bounds in enumerate(detections):
                 if frame_num == 1.0:
                     width = int(face_bounds[3]) - int(face_bounds[1])
@@ -105,73 +85,47 @@ def detect_faces(video_path):
                     #             str(frame_num) + "_" + str(i) + ".jpg", cropped)
                     cv2.imwrite(faces_video_path_inner + "/" + str(frame_num) + "_" + str(i) + ".jpg", cropped)
             print(cap.get(cv2.CAP_PROP_POS_FRAMES))
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-
-        # Break the loop
         else:
             break
-
-    # When everything done, release
-    # the video capture object
     cap.release()
-
-    # Closes all the frames
     cv2.destroyAllWindows()
-    return cap
+    return faces_video_path
 
-import torch.nn as nn
-import torch
-import torchvision.models as models
-
-
-def assign_face_ids(video_basename):
-
+# takes face groups, detects faces that are the same, and makes them into their own video
+# face_detections_path: a path to a folder that contains images of a single face
+# returns the path to the folder containing all the face videos
+def create_face_videos(face_detections_path: str) -> str:
     import torch
     import facenet_pytorch
-
-    model = facenet_pytorch.InceptionResnetV1(pretrained='vggface2').eval()
-
     import torchvision
     import torchvision.transforms as transforms
     import numpy as np
-    # tt = torchvision.transforms.Compose([
-    #     torchvision.transforms.Resize(256),
-    #     torchvision.transforms.ToTensor()]
-    # )
+    import tqdm
+    from joblib import load
+    import pandas as pd
+
+    # todo: make sure this exists
+    svm = load("compare.joblib")
+
+    model = facenet_pytorch.InceptionResnetV1(pretrained='vggface2').eval()
+
     tt = transforms.Compose([
-        transforms.Resize((160,160)),
-        # transforms.CenterCrop(224),
+        transforms.Resize((160, 160)),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     # dataset = torchvision.datasets.ImageFolder("detections/" + video_basename, transform=tt)
-    dataset = torchvision.datasets.ImageFolder(video_basename, transform=tt)
-    dataloader = torch.utils.data.DataLoader(dataset)#,sampler=np.arange(1000))
+    dataset = torchvision.datasets.ImageFolder(face_detections_path, transform=tt)
+    dataloader = torch.utils.data.DataLoader(dataset)  # ,sampler=np.arange(1000))
     anchor_embeddings = {}  # int key, tensor value
 
     found = False
     ids = np.zeros(len(dataloader))
     counter = 0
-    import tqdm
-    import matplotlib.pyplot as plt
-
-    arr = np.zeros((len(dataloader), 512)) # (512 is the embedding length
-
-    from joblib import load
-    svm = load("compare.joblib")
+    arr = np.zeros((len(dataloader), 512))  # (512 is the embedding length
 
     for idx, data in tqdm.tqdm(enumerate(dataloader)):
         embedding = model(data[0][0].unsqueeze(0).float())
-        # if idx < 10:
-        #     plt.imshow(data[0][0].numpy().transpose(1,2,0))
-        #     plt.title(str(idx))
-        #     plt.show()
         arr[idx] = embedding.detach().numpy()
-
-        ####
-
         similarities = {}
         for id, anchor_embed in anchor_embeddings.items():
             # dist = torch.norm(embedding - anchor_embed)
@@ -207,20 +161,33 @@ def assign_face_ids(video_basename):
 
         found = False
 
-        ####
-
-    import pandas as pd
     df = pd.DataFrame({'path': [x[0] for x in dataset.samples],
                        'frame': [int(os.path.basename(x[0]).split('.')[0]) for x in dataset.samples],
                        'id': ids})
-    # df = pd.DataFrame({'path': [x[0] for x in dataset.samples[:1000]],
-    #                    'frame': [int(os.path.basename(x[0]).split('.')[0]) for x in dataset.samples[:1000]],
-    #                    'id': ids})
     df = df.sort_values('frame')
     df = df.astype({'id': 'int16'})
-    return df
 
-# https://www.geeksforgeeks.org/python-play-a-video-using-opencv/
+    basename = os.path.basename(face_detections_path)
+    if not os.path.exists("cropped_face_videos"):
+        os.mkdir("cropped_face_videos")
+    face_videos_output_path = os.path.join("cropped_face_videos", basename)
+    if not os.path.exists(face_videos_output_path):
+        os.mkdir(face_videos_output_path)
+
+    for id in df['id'].unique():
+        # there may be breaks in frame continuity here (it's actually likely)
+        frame_paths = df[df['id'] == id]['path']
+        # the frames from the original video may be useful or necessary
+        # frames = df[df['id'] == id]['frame']
+        # if len(frame_paths) > 100: # todo: this doesnt always apply...
+        if len(frame_paths) > 30:
+            out_path = face_videos_output_path + "/" + str(int(id)) + ".mp4"
+            image_sequence_to_video(frame_paths, out_path)
+
+    return face_videos_output_path
+
+
+# takes a video and plays it
 def play(video_path, start_frame=0, end_frame=None):
     cap = cv2.VideoCapture(video_path)
     # Create a VideoCapture object and read from input file
@@ -260,6 +227,8 @@ def play(video_path, start_frame=0, end_frame=None):
     cv2.destroyAllWindows()
     return cap
 
+
+# takes a video, start, and end frame, and saves that bit as its own video
 def save_sub_video(video_path, start_frame, end_frame, out_path):
     cap = cv2.VideoCapture(video_path)
     # Create a VideoCapture object and read from input file
@@ -311,42 +280,37 @@ def save_sub_video(video_path, start_frame, end_frame, out_path):
     cv2.destroyAllWindows()
     return cap
 
-def change_frame_rate(video_path, frame_rate=15):
+# takes a video, and creates a new video with specified frame rate
+def change_frame_rate(video_path, out_path, frame_rate=15):
     # https://stackoverflow.com/questions/53767770/python-video-decrease-fps
     # https://stackoverflow.com/questions/29317262/opencv-video-saving-in-python
-    out = cv2.VideoWriter("test.mp4")
+    out = cv2.VideoWriter(out_path)
     cap = cv2.VideoCapture(video_path)
     cap.set(cv2.cv.CV_CAP_PROP_FPS, frame_rate)
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-            # frame = cv2.flip(frame, 0)
-
-            # write the flipped frame
             out.write(frame)
-
-            # cv2.imshow('frame', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
         else:
             break
-
     # Release everything if job is finished
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
-
+# creates a face verification model
 def train_svm_on_lfw():
     from sklearn.datasets import fetch_lfw_pairs
-    stuff = fetch_lfw_pairs()
     import torchvision
     import torchvision.transforms as transforms
     import numpy as np
-    # tt = torchvision.transforms.Compose([
-    #     torchvision.transforms.Resize(256),
-    #     torchvision.transforms.ToTensor()]
-    # )
+    import facenet_pytorch
+    from PIL import Image
+    import tqdm
+    from sklearn.svm import SVC
+    from joblib import dump
+
+    stuff = fetch_lfw_pairs()
     tt = transforms.Compose([
         transforms.Resize((160,160)),
         # transforms.CenterCrop(224),
@@ -354,9 +318,7 @@ def train_svm_on_lfw():
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     arr = np.zeros((len(stuff.pairs), 512))
-    import facenet_pytorch
-    from PIL import Image
-    import tqdm
+
     model = facenet_pytorch.InceptionResnetV1(pretrained='vggface2').eval()
     for idx, val in tqdm.tqdm(enumerate(stuff.pairs)):
         em1 = model(tt(Image.fromarray(val[0]).convert("RGB")).view(1,3,160,160)) #...something like that
@@ -364,18 +326,17 @@ def train_svm_on_lfw():
         ee = em1 - em2
         arr[idx] = ee.detach().numpy()
 
-    from sklearn.svm import SVC
     fit = SVC().fit(arr, stuff.target)
-    from joblib import dump
+
     dump(fit, "compare.joblib")
 
     return arr, stuff.target
 
-def make_aligned_video(frame_paths, out_path):
+# takes a series of image paths and concatenates them into a video
+def image_sequence_to_video(frame_paths, out_path):
     # (assuming all samples are the same size)
     sample = cv2.imread(frame_paths[0])
     out_dim = (sample.shape[1], sample.shape[0])
-    #
 
     out = cv2.VideoWriter(out_path,
                           cv2.VideoWriter_fourcc(*'mp4v'),
@@ -387,25 +348,3 @@ def make_aligned_video(frame_paths, out_path):
         a = cv2.resize(a, out_dim)
         out.write(a)
     out.release()
-
-
-def pls(files_path):
-    # files_path = "A1_AU1_TrailNo_2"
-    # li = []
-    # for video in sorted(os.listdir(files_path)):
-    #     li.append(os.path.join(files_path, video))
-    # make_aligned_video(li, "videos/pls.mp4")
-
-    import os
-    base_paths = ["SN001", "SN003", "SN004", "SN007", "SN009", "SN010",
-                  "SN013", "SN025", "SN027"]
-    for base_path in base_paths:
-        for path in os.listdir(base_path):
-            li = []
-            full_path = os.path.join(base_path, path)
-            if os.path.isdir(full_path):
-                for video in sorted(os.listdir(full_path)):
-                    if ".jpg" in video:
-                        li.append(os.path.join(full_path, video))
-                print(li)
-                videos.make_aligned_video(li, "videos/" + base_path + path + ".mp4")
